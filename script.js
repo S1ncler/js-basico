ed420c59c07f460bee026c6eda44db7393aaf385
S1ncler <daniel_felipe_c11@hotmail.com>
2023-03-28 20:52:50 -0500

    Clase 28 de marzo commit para cambio a windows

---
+++ script.js
@@ -0,0 +1,31 @@
+/* tipos de datos*/
+// numericos enteros o flotantes
+let numero = 21335423.345;
+// bool true || false
+let booleano = true;
+// listas o arreglos [] 
+let arreglo = ["hola", "mundo" , 2, 3, [4, 5]];
+// objetos json js object notation      
+// persona{
+//    nombre: "Max"
+//    apelllito: "Power"
+//    llave: valor
+//  } son objetos que fucionan con una llave y un valor 
+let valor = "variable";
+let persona = {
+    nombre: "Max",
+    apellido: "power",
+    llave: valor
+}
+// null
+let nulo = null;
+// undefined
+let indefinido = undefined;
+
+let nada;
+console.log(numero, booleano, arreglo, valor, persona.nombre, nulo, indefinido, nada);
+if(variabe === 0){
+    
+}else{
+
+}
