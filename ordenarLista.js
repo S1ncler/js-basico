f3f37ec8fa9f564fb62ea62b83f14b5689d56570
S1ncler <daniel_felipe_c11@hotmail.com>
2023-04-14 21:42:23 -0500

    Codigo un poquito optimizado del ordenar lista

--- ordenarLista.js
+++ ordenarLista.js
@@ -1,9 +1,9 @@
 
-let lista = [45, 23, 78, 32, 12, 1, 4, 8, 34, 32, 1000, 34, 37, 22, 0, -4, -6]
+let lista = [23, 45, 78, 32, 12, 1, 4, 8, 34, 32, 1000, 34, 37, 22, 0, -4, -6]
 
 function organizar(lista) {
-    for (let i = 0; i < lista.length - 1; i++) {
-        for (let j = 0; j < lista.length - i - 1; j++) {
+    for (let i of lista) {
+        for (let j in lista) {
             if (lista[j] > lista[parseInt(j) + 1]) {
                 let carry = lista[parseInt(j) + 1]
                 lista[parseInt(j) + 1] = lista[j]
